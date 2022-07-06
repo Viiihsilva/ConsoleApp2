248b3a29e8e5ed84c4bec3aa3aeb8e3114799be2
Victoria Silva <Victoria Silva@DESKTOP-TP3RSDB>
2022-07-06 19:49:29 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s;
+            for (s = 1; s <= 50;)
+            {
+                Console.WriteLine(s);
+                s++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
